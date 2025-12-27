f6d357aece70754a779527efa71212a368ad4be2
Allan Cordeiro <allan.galdino@seplag.ce.gov.br>
2025-12-27 20:30:23 -0300

    implementando controller: get e post

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,12 +1,23 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("customer")
 class CustomerController {
     @GetMapping
-    fun HelloWorld(): String {
-        return "Hello Worlda"
+    fun getCustomer(): CustomerModel {
+        return CustomerModel("1", "Allan", "allangdc@gmail.com")
+    }
+
+    @PostMapping
+    fun create(@RequestBody customer: PostCustomerRequest) {
+        println(customer)
     }
 }
